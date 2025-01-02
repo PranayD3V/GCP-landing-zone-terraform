f13dc6a140f29e8c9f417ce27080d63ad4f01a4f
Pranay <pranaypaunikar48@gmail.com>
2025-01-02 16:36:26 +0530

    vertex-ai

--- vertex_ai.tf
+++
@@ -1,6 +0,0 @@
-resource "google_vertex_ai_dataset" "dataset" {
-  display_name          = "terraform"
-  metadata_schema_uri   = "gs://google-cloud-aiplatform/schema/dataset/metadata/image_1.0.0.yaml"
-  region                = "us-central1"
-  project = module.cs-svc-nonprod1-svc-j8yh.project_id
-}
